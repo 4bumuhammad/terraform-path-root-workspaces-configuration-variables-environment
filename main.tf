0e888d2498d0054c5529b06fa970f980f401086a
Dhony Abu Muhammad <76083017+4bumuhammad@users.noreply.github.com>
2024-04-08 00:35:20 +0700

    updates

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "null_resource" "make_directory" {
   triggers = {
-    always_run = "${timestamp()}"
+    always_run   = "${timestamp()}"
+    trigger_name = "trigger-make-directory"
   }
 
   provisioner "local-exec" {
@@ -11,7 +12,28 @@ resource "null_resource" "make_directory" {
       USERDIRECTORY = var.user_directory
     }
   }
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "REPORTS" > directory_name.txt
+      echo "  Trigger Name: ${self.triggers.trigger_name}" >> directory_name.txt
+      echo "  Terraform Workspace: $(terraform workspace show)" >> directory_name.txt
+      echo "  Environment USERDIRECTORY: ${var.user_directory}" >> directory_name.txt
+      echo "  Created Directory: ${local.root_directory}/${local.folder_types[terraform.workspace]}-dir-${var.user_directory}" >> directory_name.txt
+      echo "  Timestamp: $(date)" >> directory_name.txt
+    EOT
+  }
+
 }
+data "local_file" "load_directory_name" {
+  depends_on = [null_resource.make_directory]
+  filename   = "directory_name.txt"
+}
+
+output "load_directory_name" {
+  value = data.local_file.load_directory_name.content
+}
+
+
 
 resource "null_resource" "create_file" {
   depends_on = [null_resource.make_directory]
